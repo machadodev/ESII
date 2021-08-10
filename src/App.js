72328d0d23ef516eb6f2355df2db51b488ceea19
Leonardo Machado <leonardoms@id.uff.br>
2021-08-10 20:57:00 -0300

    adicionada logo na tela inicial
    Co-authored-by: Hiaggo Bezerra <Hiaggo@users.noreply.github.com>
    Co-authored-by: Carlos Eduardo Gaudie Ley Menezes Junior <CeMenezesJunior@users.noreply.github.com>
    Co-authored-by: Alisson Weiss <AlissonWeiss@users.noreply.github.com>
    Co-authored-by: Jorge Guimaraes <jmguimaraes@id.uff.br>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
+import Home from './pages/Home'
 
 function App () {
   return (
-    <div className="App bg-black">
-      <div className="h-screen bg-war bg-cover bg-no-repeat bg-center brightness-50">
-      <p>learn react</p>
-      </div>
-    </div>
+    <Home/>
   )
 }
 
