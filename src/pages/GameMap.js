61cf54a69a54d6adcf1b6870255087c01f3f2a7b
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-09-06 20:38:45 -0300

    add: hello code

---
+++ src/pages/GameMap.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function GameMap () {
+  return (
+    <div className="App">
+      <section className="h-100">
+        <header className="container h-100">
+          <div className="d-flex align-items-center justify-content-center h-100">
+            <div className="d-flex flex-column">
+            <div>
+              <h1 className="title is-1">This is the About Page</h1>
+              <p>
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus
+                et ultrices posuere cubilia curae; Duis consequat nulla ac ex consequat,
+                in efficitur arcu congue. Nam fermentum commodo egestas.
+              </p>
+            </div>
+            </div>
+          </div>
+        </header>
+      </section>
+    </div>
+  )
+}
+
+export default GameMap
