00349cfbbd50ed382fef746bb53a786814488368
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-09-06 20:37:26 -0300

    feat: introduzir sistema de rotas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './custom.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './custom.scss'
+import App from './App'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
+import Home from './pages/Home'
+import GameMap from './pages/GameMap'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+  <Router>
+    <Switch>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      <Route exact path="/" component={ Home }/>,
+      <Route exact path="/game" component={ GameMap }/>
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
