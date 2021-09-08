9f02a8d3612663c6464da82a69b2c9ad8995067f
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-09-08 00:45:44 -0300

    feat: mostar etados nos player cards #34

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Container, Col, ListGroup, Row } from 'react-bootstrap';
 import PlayerIcon from './PlayerIcon';
+import { ESTADOS_DA_FEDERACAO } from '../helper/CONSTANTS';
+import EstadoFederacao from './EstadoFederacao';
 
 function Player(props) {
   const { playerInfo } = props;
-
+  // TODO: estadosInfo deve vir via props
+  /*
+    Este dado deve ser servido pelo GameManager, deve ser responsabilidade
+    dele gerenciar estes dados, pois é ele quem vai  manipular as operações
+    de aquisição ou perda de estados
+  */
+  const estadosInfo = [
+    {
+      estado: ESTADOS_DA_FEDERACAO.rj,
+      exercitos: 3,
+    },
+    {
+      estado: ESTADOS_DA_FEDERACAO.sp,
+      exercitos: 4,
+    },
+  ];
   return (
     <ListGroup.Item as="li" variant={playerInfo.color.variant}>
       {/* TODO: Add EstadoFederacao */}
@@ -15,6 +32,13 @@ function Player(props) {
             <PlayerIcon archetype={playerInfo.archetype} />
           </Col>
           <Col xs={10}> Exército {playerInfo.color.name}</Col>
+          {estadosInfo.map((estadoInfo) => (
+            <EstadoFederacao
+              estadoInfo={estadoInfo}
+              playerInfo={playerInfo}
+              key={estadoInfo.estado.idEstado}
+            />
+          ))}
         </Row>
       </Container>
     </ListGroup.Item>
