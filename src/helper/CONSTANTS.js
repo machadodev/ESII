f7b7ffb1e143049f65a0e558644d0f1f5aca189c
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-09-07 21:41:14 -0300

    feat: instanciar game manager #34

--- src/helper/CONSTANTS.js
+++ src/helper/CONSTANTS.js
@@ -1,4 +1,182 @@
+const ESTADOS_DA_FEDERACAO = {
+  UF: [
+    {
+      nome: 'Acre',
+      sigla: 'AC',
+      divisas: [''],
+    },
+    {
+      nome: 'Alagoas',
+      sigla: 'AL',
+      divisas: [''],
+    },
+    {
+      nome: 'Amapá',
+      sigla: 'AP',
+      divisas: [''],
+    },
+    {
+      nome: 'Amazonas',
+      sigla: 'AM',
+      divisas: [''],
+    },
+    {
+      nome: 'Bahia',
+      sigla: 'BA',
+      divisas: [''],
+    },
+    {
+      nome: 'Ceará',
+      sigla: 'CE',
+      divisas: [''],
+    },
+    {
+      nome: 'Distrito Federal',
+      sigla: 'DF',
+      divisas: [''],
+    },
+    {
+      nome: 'Espírito Santo',
+      sigla: 'ES',
+      divisas: [''],
+    },
+    {
+      nome: 'Goiás',
+      sigla: 'GO',
+      divisas: [''],
+    },
+    {
+      nome: 'Maranhão',
+      sigla: 'MA',
+      divisas: [''],
+    },
+    {
+      nome: 'Mato Grosso',
+      sigla: 'MT',
+      divisas: [''],
+    },
+    {
+      nome: 'Mato Grosso do Sul',
+      sigla: 'MS',
+      divisas: [''],
+    },
+    {
+      nome: 'Minas Gerais',
+      sigla: 'MG',
+      divisas: [''],
+    },
+    {
+      nome: 'Pará',
+      sigla: 'PA',
+      divisas: [''],
+    },
+    {
+      nome: 'Paraíba',
+      sigla: 'PB',
+      divisas: [''],
+    },
+    {
+      nome: 'Paraná',
+      sigla: 'PR',
+      divisas: [''],
+    },
+    {
+      nome: 'Pernambuco',
+      sigla: 'PE',
+      divisas: [''],
+    },
+    {
+      nome: 'Piauí',
+      sigla: 'PI',
+      divisas: [''],
+    },
+    {
+      nome: 'Rio de Janeiro',
+      sigla: 'RJ',
+      divisas: [''],
+    },
+    {
+      nome: 'Rio Grande do Norte',
+      sigla: 'RN',
+      divisas: [''],
+    },
+    {
+      nome: 'Rio Grande do Sul',
+      sigla: 'RS',
+      divisas: [''],
+    },
+    {
+      nome: 'Rondônia',
+      sigla: 'RO',
+      divisas: [''],
+    },
+    {
+      nome: 'Roraima',
+      sigla: 'RR',
+      divisas: [''],
+    },
+    {
+      nome: 'Santa Catarina',
+      sigla: 'SC',
+      divisas: [''],
+    },
+    {
+      nome: 'São Paulo',
+      sigla: 'SP',
+      divisas: [''],
+    },
+    {
+      nome: 'Sergipe',
+      sigla: 'SE',
+      divisas: [''],
+    },
+    {
+      nome: 'Tocantins',
+      sigla: 'TO',
+      divisas: [''],
+    },
+  ],
+};
+
 const MAX_PLAYERS_IN_SESSION = 6;
 const MIN_PLAYERS_TO_START_GAME = 3;
 
-export { MAX_PLAYERS_IN_SESSION, MIN_PLAYERS_TO_START_GAME };
+const PLAYER_MATCH_INFO = {
+  blue: {
+    index: 0,
+    name: 'Azul',
+    variant: 'primary',
+  },
+  black: {
+    index: 1,
+    name: 'Preto',
+    variant: 'secondary',
+  },
+  green: {
+    index: 2,
+    name: 'Verde',
+    variant: 'success',
+  },
+  red: {
+    index: 3,
+    name: 'Vermelho',
+    variant: 'danger',
+  },
+  yellow: {
+    index: 4,
+    name: 'Amarelo',
+    variant: 'warning',
+  },
+  white: {
+    index: 5,
+    name: 'Branco',
+    variant: 'light',
+  },
+};
+
+export {
+  ESTADOS_DA_FEDERACAO,
+  MAX_PLAYERS_IN_SESSION,
+  MIN_PLAYERS_TO_START_GAME,
+  PLAYER_MATCH_INFO,
+};
--- src/pages/GameMap.js
+++ src/pages/GameMap.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import GameManager from '../components/GameManager';
 import MapaBrasil from '../components/MapaBrasil';
+import { PLAYER_MATCH_INFO } from '../helper/CONSTANTS';
 
 function GameMap() {
+  const match = {
+    players: [
+      { id: 1, color: PLAYER_MATCH_INFO.red, playerType: 'human' },
+      { id: 2, color: PLAYER_MATCH_INFO.green, playerType: 'ia' },
+      { id: 3, color: PLAYER_MATCH_INFO.white, playerType: 'ia' },
+    ],
+  };
+
   return (
     <div className="App">
       <section className="h-100">
         <header className="container h-100">
           <div className="d-flex align-items-center justify-content-center h-100">
             <div className="d-flex flex-column">
+              <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+                <GameManager match={match} />
+              </nav>
               <main className="content-wrapper">
                 <div className="container-fluid">
                   <MapaBrasil />
