cfc4713b6e91cb2ea2e8b93205356e153bbbadc6
Jorge M Guimaraes <jmguimaraes@id.uff.br>
2021-09-08 19:59:01 -0300

    fix: remover TODO

--- src/components/MapaEstado.js
+++ src/components/MapaEstado.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 
-// TODO: falta fazer o teste
 function MapaEstado(props) {
   const { estado } = props;
   return <Image alt={estado.nome} src={estado.image} width="100px" />;
