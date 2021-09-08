687c8b7d622cda81b4d37447bc99bfab872955ca
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-09-08 00:41:56 -0300

    feat: guardar imagens dos estados #34

--- src/helper/CONSTANTS.js
+++ src/helper/CONSTANTS.js
@@ -1,141 +1,215 @@
+import AC from '../assets/images/maps/ac.png';
+import AL from '../assets/images/maps/al.png';
+import AP from '../assets/images/maps/ap.png';
+import AM from '../assets/images/maps/am.png';
+import BA from '../assets/images/maps/ba.png';
+import CE from '../assets/images/maps/ce.png';
+import ES from '../assets/images/maps/es.png';
+import GO from '../assets/images/maps/go.png';
+import MA from '../assets/images/maps/ma.png';
+import MT from '../assets/images/maps/mt.png';
+import MS from '../assets/images/maps/ms.png';
+import MG from '../assets/images/maps/mg.png';
+import PA from '../assets/images/maps/pa.png';
+import PB from '../assets/images/maps/pb.png';
+import PR from '../assets/images/maps/pr.png';
+import PE from '../assets/images/maps/pe.png';
+import PI from '../assets/images/maps/pi.png';
+import RJ from '../assets/images/maps/rj.png';
+import RN from '../assets/images/maps/rn.png';
+import RS from '../assets/images/maps/rs.png';
+import RO from '../assets/images/maps/ro.png';
+import RR from '../assets/images/maps/rr.png';
+import SC from '../assets/images/maps/sc.png';
+import SP from '../assets/images/maps/sp.png';
+import SE from '../assets/images/maps/se.png';
+import TO from '../assets/images/maps/to.png';
+// TODO: se forem utilizar estas divisas, falta preencher
+// Eu acho prático
+
 const ESTADOS_DA_FEDERACAO = {
-  UF: [
-    {
-      nome: 'Acre',
-      sigla: 'AC',
-      divisas: [''],
-    },
-    {
-      nome: 'Alagoas',
-      sigla: 'AL',
-      divisas: [''],
-    },
-    {
-      nome: 'Amapá',
-      sigla: 'AP',
-      divisas: [''],
-    },
-    {
-      nome: 'Amazonas',
-      sigla: 'AM',
-      divisas: [''],
-    },
-    {
-      nome: 'Bahia',
-      sigla: 'BA',
-      divisas: [''],
-    },
-    {
-      nome: 'Ceará',
-      sigla: 'CE',
-      divisas: [''],
-    },
-    {
-      nome: 'Distrito Federal',
-      sigla: 'DF',
-      divisas: [''],
-    },
-    {
-      nome: 'Espírito Santo',
-      sigla: 'ES',
-      divisas: [''],
-    },
-    {
-      nome: 'Goiás',
-      sigla: 'GO',
-      divisas: [''],
-    },
-    {
-      nome: 'Maranhão',
-      sigla: 'MA',
-      divisas: [''],
-    },
-    {
-      nome: 'Mato Grosso',
-      sigla: 'MT',
-      divisas: [''],
-    },
-    {
-      nome: 'Mato Grosso do Sul',
-      sigla: 'MS',
-      divisas: [''],
-    },
-    {
-      nome: 'Minas Gerais',
-      sigla: 'MG',
-      divisas: [''],
-    },
-    {
-      nome: 'Pará',
-      sigla: 'PA',
-      divisas: [''],
-    },
-    {
-      nome: 'Paraíba',
-      sigla: 'PB',
-      divisas: [''],
-    },
-    {
-      nome: 'Paraná',
-      sigla: 'PR',
-      divisas: [''],
-    },
-    {
-      nome: 'Pernambuco',
-      sigla: 'PE',
-      divisas: [''],
-    },
-    {
-      nome: 'Piauí',
-      sigla: 'PI',
-      divisas: [''],
-    },
-    {
-      nome: 'Rio de Janeiro',
-      sigla: 'RJ',
-      divisas: [''],
-    },
-    {
-      nome: 'Rio Grande do Norte',
-      sigla: 'RN',
-      divisas: [''],
-    },
-    {
-      nome: 'Rio Grande do Sul',
-      sigla: 'RS',
-      divisas: [''],
-    },
-    {
-      nome: 'Rondônia',
-      sigla: 'RO',
-      divisas: [''],
-    },
-    {
-      nome: 'Roraima',
-      sigla: 'RR',
-      divisas: [''],
-    },
-    {
-      nome: 'Santa Catarina',
-      sigla: 'SC',
-      divisas: [''],
-    },
-    {
-      nome: 'São Paulo',
-      sigla: 'SP',
-      divisas: [''],
-    },
-    {
-      nome: 'Sergipe',
-      sigla: 'SE',
-      divisas: [''],
-    },
-    {
-      nome: 'Tocantins',
-      sigla: 'TO',
-      divisas: [''],
-    },
-  ],
+  ac: {
+    idEstado: 1,
+    nome: 'Acre',
+    sigla: 'AC',
+    divisas: ['am'],
+    image: AC,
+  },
+  al: {
+    idEstado: 2,
+    nome: 'Alagoas',
+    sigla: 'AL',
+    divisas: ['se', 'ba', 'pe'],
+    image: AL,
+  },
+  ap: {
+    idEstado: 3,
+    nome: 'Amapá',
+    sigla: 'AP',
+    divisas: [''],
+    image: AP,
+  },
+  am: {
+    idEstado: 4,
+    nome: 'Amazonas',
+    sigla: 'AM',
+    divisas: [''],
+    image: AM,
+  },
+  ba: {
+    idEstado: 5,
+    nome: 'Bahia',
+    sigla: 'BA',
+    divisas: [''],
+    image: BA,
+  },
+  ce: {
+    idEstado: 6,
+    nome: 'Ceará',
+    sigla: 'CE',
+    divisas: [''],
+    image: CE,
+  },
+  es: {
+    idEstado: 8,
+    nome: 'Espírito Santo',
+    sigla: 'ES',
+    divisas: [''],
+    image: ES,
+  },
+  go: {
+    idEstado: 9,
+    nome: 'Goiás',
+    sigla: 'GO',
+    divisas: [''],
+    image: GO,
+  },
+  ma: {
+    idEstado: 10,
+    nome: 'Maranhão',
+    sigla: 'MA',
+    divisas: [''],
+    image: MA,
+  },
+  mt: {
+    idEstado: 11,
+    nome: 'Mato Grosso',
+    sigla: 'MT',
+    divisas: [''],
+    image: MT,
+  },
+  ms: {
+    idEstado: 12,
+    nome: 'Mato Grosso do Sul',
+    sigla: 'MS',
+    divisas: [''],
+    image: MS,
+  },
+  mg: {
+    idEstado: 13,
+    nome: 'Minas Gerais',
+    sigla: 'MG',
+    divisas: [''],
+    image: MG,
+  },
+  pa: {
+    idEstado: 14,
+    nome: 'Pará',
+    sigla: 'PA',
+    divisas: [''],
+    image: PA,
+  },
+  pb: {
+    idEstado: 15,
+    nome: 'Paraíba',
+    sigla: 'PB',
+    divisas: [''],
+    image: PB,
+  },
+  pr: {
+    idEstado: 16,
+    nome: 'Paraná',
+    sigla: 'PR',
+    divisas: [''],
+    image: PR,
+  },
+  pe: {
+    idEstado: 17,
+    nome: 'Pernambuco',
+    sigla: 'PE',
+    divisas: [''],
+    image: PE,
+  },
+  pi: {
+    idEstado: 18,
+    nome: 'Piauí',
+    sigla: 'PI',
+    divisas: [''],
+    image: PI,
+  },
+  rj: {
+    idEstado: 19,
+    nome: 'Rio de Janeiro',
+    sigla: 'RJ',
+    divisas: [''],
+    image: RJ,
+  },
+  rn: {
+    idEstado: 20,
+    nome: 'Rio Grande do Norte',
+    sigla: 'RN',
+    divisas: [''],
+    image: RN,
+  },
+  rs: {
+    idEstado: 21,
+    nome: 'Rio Grande do Sul',
+    sigla: 'RS',
+    divisas: [''],
+    image: RS,
+  },
+  ro: {
+    idEstado: 22,
+    nome: 'Rondônia',
+    sigla: 'RO',
+    divisas: [''],
+    image: RO,
+  },
+  rr: {
+    idEstado: 23,
+    nome: 'Roraima',
+    sigla: 'RR',
+    divisas: [''],
+    image: RR,
+  },
+  sc: {
+    idEstado: 24,
+    nome: 'Santa Catarina',
+    sigla: 'SC',
+    divisas: [''],
+    image: SC,
+  },
+  sp: {
+    idEstado: 25,
+    nome: 'São Paulo',
+    sigla: 'SP',
+    divisas: [''],
+    image: SP,
+  },
+  se: {
+    idEstado: 26,
+    nome: 'Sergipe',
+    sigla: 'SE',
+    divisas: [''],
+    image: SE,
+  },
+  to: {
+    idEstado: 27,
+    nome: 'Tocantins',
+    sigla: 'TO',
+    divisas: [''],
+    image: TO,
+  },
 };
 
 const MAX_PLAYERS_IN_SESSION = 6;
