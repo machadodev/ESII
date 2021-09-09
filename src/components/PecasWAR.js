09f836c2fc6986e12c08d9080fd1580553328e39
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-09-08 23:40:04 -0300

    Criação da lógica do número de peças com teste

---
+++ src/components/PecasWAR.js
@@ -0,0 +1,4 @@
+function LogicaPecas (numTerritorios) {
+  return numTerritorios === 1 ? numTerritorios : numTerritorios / 2
+}
+export default LogicaPecas
---
+++ src/components/PecasWar.test.js
@@ -0,0 +1,5 @@
+import LogicaPecas from './PecasWAR'
+
+test('Logical Test', () => {
+  expect(LogicaPecas(2)).toBeLessThanOrEqual(1)
+})
