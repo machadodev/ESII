ce9f3237f720b08d6beb4319e54801ad0a430c25
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-09-08 23:48:11 -0300

    Ajuste para atender as rodadas

--- src/components/PecasWAR.js
+++ src/components/PecasWAR.js
@@ -1,4 +1,7 @@
-function LogicaPecas (numTerritorios) {
+function LogicaPecas (rodada, numTerritorios) {
+  if (rodada === 1) {
+    return numTerritorios
+  }
   return numTerritorios === 1 ? numTerritorios : numTerritorios / 2
 }
 export default LogicaPecas
