17b76d3347a0662e3b06935ca7ba80e04d2f12f1
Jorge Guimaraes <jmguimaraes@id.uff.br>
2021-09-11 00:43:21 -0300

    feat: exibir modal de objetivos #42

--- src/pages/GameMap.js
+++ src/pages/GameMap.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import GameManager from '../components/GameManager';
 import MapaBrasil from '../components/MapaBrasil';
-import { PLAYER_MATCH_INFO } from '../helper/CONSTANTS';
+import { OBJECTIVES, PLAYER_MATCH_INFO } from '../helper/CONSTANTS';
+import SorteioObjetivo from '../components/SorteioObjetivo';
 
 function GameMap() {
   const match = {
     players: [
-      { id: 1, color: PLAYER_MATCH_INFO.red, archetype: 'human' },
-      { id: 2, color: PLAYER_MATCH_INFO.green, archetype: 'ia' },
-      { id: 3, color: PLAYER_MATCH_INFO.white, archetype: 'ia' },
+      {
+        id: 1,
+        color: PLAYER_MATCH_INFO.red,
+        archetype: 'human',
+        objective: OBJECTIVES.sul,
+      },
+      {
+        id: 2,
+        color: PLAYER_MATCH_INFO.green,
+        archetype: 'ia',
+        objective: OBJECTIVES.litoral,
+      },
+      {
+        id: 3,
+        color: PLAYER_MATCH_INFO.white,
+        archetype: 'ia',
+        objective: OBJECTIVES.floresta,
+      },
     ],
   };
 
@@ -19,7 +35,8 @@ function GameMap() {
           <div className="d-flex align-items-center justify-content-center h-100">
             <div className="d-flex flex-column">
               <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-                <h1 className="navbar-brand">War: Mapa do Brasil</h1>
+                <h1 className="navbar-brand">War: Mapa do Brasil </h1>
+                <SorteioObjetivo match={match} />
                 <GameManager match={match} />
               </nav>
               <main className="content-wrapper">
