1dd7e2b5c2c0ca9be0b79b6b13b2ce6ca78202b7
Leonardo Machado <leonardoms@id.uff.br>
2021-09-14 19:23:51 -0300

    feature: Dividir Estados entre jogadores closed #13
    Co-authored-by: Hiaggo Bezerra <Hiaggo@users.noreply.github.com>

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import Player from './Player';
-import { ESTADOS_DA_FEDERACAO } from '../helper/CONSTANTS';
+import DividirEstadosFederacao from '../usecase/DividirEstadosFederacao';
 
 function GameManager(props) {
   const { match } = props;
 
-  const estados = Object.values(ESTADOS_DA_FEDERACAO);
-
-  const shuffled = estados
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
-
-  const splitToChunks = (array, parts) => {
-    const result = [];
-    for (let i = parts; i > 0; i -= 1) {
-      result.push(array.splice(0, Math.ceil(array.length / i)));
-    }
-    return result;
-  };
-
-  const splitted = splitToChunks(shuffled, match.players.length);
+  const estadosParticionados = DividirEstadosFederacao(match.players.length);
 
   match.players = match.players.map((player) => ({
     ...player,
-    estados: splitted.shift(),
+    estados: estadosParticionados.shift(),
     exercito: 0,
   }));
 
