a2147004a72277328e34b741f7f3c996030135b6
Leonardo Machado <leonardoms@id.uff.br>
2021-09-14 21:17:00 -0300

    removidos

--- src/components/PecasWAR.js
+++
@@ -1,7 +0,0 @@
-function LogicaPecas (rodada, numTerritorios) {
-  if (rodada === 1) {
-    return numTerritorios
-  }
-  return numTerritorios === 1 ? numTerritorios : numTerritorios / 2
-}
-export default LogicaPecas
--- src/components/PecasWar.test.js
+++
@@ -1,5 +0,0 @@
-import LogicaPecas from './PecasWAR'
-
-test('Logical Test', () => {
-  expect(LogicaPecas(1, 2)).toBeLessThanOrEqual(2)
-})
